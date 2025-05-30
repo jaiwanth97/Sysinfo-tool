6cd62bce689a81fa42b03c48d9fc979d229302fb
jaiwanth97 <jaiwanth97@gmail.com>
2025-05-30 19:50:46 +0530

    Initial commit

---
+++ sysinfo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+while true; do
+    echo "                             "
+    echo "============================="
+    echo "=     Simple SysInfo        ="
+    echo "============================="
+    echo "      1.CPU Usage            "
+    echo "      2.Memory               "
+    echo "      3.Disk Usage           "
+    echo "      4.Uptime               "
+    echo "      5.Quit                 "
+    echo "                             "
+    echo "     Silly tool by jaiwanth:)"
+    echo "                             "
+    read -p "Enter  Choice [1-5]: " choice
+
+case $choice in
+        1)
+                echo "CPU Usage"
+                top -i |grep "Cpu(s)"
+                ;;
+        2)
+                echo "Memory Usage"
+                free -m
+                ;;
+        3)
+                echo "Disk Usage"
+                df -h
+                ;;
+        4)
+                echo "Uptime"
+                echo "PC is up from $(uptime -s) and up for $(uptime -p)"
+                ;;
+        5)
+                echo "Goodbye"
+                break
+                ;;
+esac
+done
